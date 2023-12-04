31a35be2bbd6b69fa0d9064fa2d7e2a11c7ba68e
Aishwarya Honap <aishwarya.honap@gmail.com>
2023-12-03 19:21:51 -0500

    Updates

--- app.js
+++ app.js
@@ -8,9 +8,40 @@ mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 import UserRoutes from "./users/routes.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import session from "express-session";
 const app = express()
 
-app.use(cors());
+app.use(
+    cors({
+      credentials: true,
+    //   origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL
+    })
+   );
+
+// const sessionOptions = {
+//     secret: "any string",
+//     resave: false,
+//     saveUninitialized: false,
+//   };
+// app.use(
+//     session(sessionOptions)
+// );
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+app.use(session(sessionOptions));
+  
+  
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
