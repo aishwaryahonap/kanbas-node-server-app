88d6189da57baf07cd482dcd56c8591e4660b052
Aishwarya Honap <aishwarya.honap@gmail.com>
2023-12-03 21:40:34 -0500

    Updates 4

--- app.js
+++ app.js
@@ -4,20 +4,24 @@ import Hello from "./hello.js"
 import Lab5 from "./Lab5.js";
 import cors from "cors";
 import mongoose from "mongoose";
-const CONNECTION_STRING = 'mongodb+srv://aishwaryahonap:aishwaryahonap1861@cluster0.gznzevi.mongodb.net/?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/kanbas'
-// const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/kanbas'
+//const CONNECTION_STRING = 'mongodb+srv://aishwaryahonap:aishwaryahonap1861@cluster0.gznzevi.mongodb.net/?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/kanbas'
+ //const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/kanbas'
+ const CONNECTION_STRING = 'mongodb+srv://aishwaryahonap:aishwaryahonap1861@cluster0.gznzevi.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(CONNECTION_STRING);
 import UserRoutes from "./users/routes.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import session from "express-session";
+//const API=process.env.FRONTEND_URL
 const app = express()
 
 app.use(
     cors({
       credentials: true,
-    //   origin: "http://localhost:3000",
+     // origin: "http://localhost:3000",
     origin: process.env.FRONTEND_URL
+     //origin: API
+
     })
    );
 
